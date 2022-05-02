1e4b629d9d15de1877e8ea3db271803e9e9d64cd
Pierre Marijon <pierre.marijon-ext@aphp.fr>
2022-05-02 17:00:22 +0200

    Group is a optional for dynamic_scatter_plot

--- snakemake_ba/plot.py
+++ snakemake_ba/plot.py
@@ -1,6 +1,7 @@
 """Use snakemake benchmark data and plot them"""
 
 # std import
+import typing
 
 import altair  # type: ignore
 
@@ -9,20 +10,18 @@ import pandas  # type: ignore
 
 
 def dynamic_scatter_plot(
-    df: pandas.DataFrame, x: str, y: str, group: str
+    df: pandas.DataFrame, x: str, y: str, group: typing.Optional[str] = None
 ) -> altair.Chart:
     """Generate an altair dynamic scatter plot
 
     Args:
         x (str): data show on x axis
         y (str): data show on y axis
-        group (str): dataframe column use to group data
+        group (str): dataframe column use to group data (optional)
 
     Returns:
         chart (altair.Chart): scatter plot
     """
-
-    selection = altair.selection_multi(fields=[group], bind="legend")
 
     chart = (
         altair.Chart(df)
@@ -30,12 +29,17 @@ def dynamic_scatter_plot(
         .encode(
             x=x,
             y=y,
-            color=group,
-            opacity=altair.condition(selection, altair.value(1), altair.value(0.1)),
         )
-        .add_selection(selection)
         .properties(width=800, height=600)
         .interactive()
     )
+
+    if group is not None:
+        selection = altair.selection_multi(fields=[group], bind="legend")
+
+        chart = chart.encode(
+            color=group,
+            opacity=altair.condition(selection, altair.value(1), altair.value(0.1)),
+        ).add_selection(selection)
 
     return chart
